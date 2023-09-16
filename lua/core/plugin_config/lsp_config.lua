c8ee5e8a3e0514f0446bc1c06477c92e61169782
lanerLF <135287624+lanerLF@users.noreply.github.com>
2023-09-16 02:20:49 -0300

    adds emmet_ls for webdev shenanigans

--- lua/core/plugin_config/lsp_config.lua
+++ lua/core/plugin_config/lsp_config.lua
@@ -9,6 +9,20 @@ end
 
 --local capabilities = vim.lsp.protocol.make_client_capabilities()
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+require("lspconfig").emmet_ls.setup {
+  on_attach = on_attach,
+  capabilities = capabilities,
+  filetypes = { "css", "eruby", "html", "javascript", "javascriptreact", "less", "sass", "scss", "svelte", "pug", "typescriptreact", "vue" },
+  init_options = {
+    html = {
+      options = {
+        ["bem.enabled"] = true,
+      },
+    },
+  }
+}
+
 require("lspconfig").lua_ls.setup {
   on_attach = on_attach,
   capabilities = capabilities,
@@ -16,8 +30,22 @@ require("lspconfig").lua_ls.setup {
     Lua = {
       diagnostics = {
         globals = { "vim" },
-
       },
     },
   },
+}
+
+require("lspconfig").html.setup {
+  on_attach = on_attach,
+  capabilities = capabilities,
+}
+
+require("lspconfig").volar.setup {
+  on_attach = on_attach,
+  capabilities = capabilities,
+}
+
+require("lspconfig").cssls.setup {
+  on_attach = on_attach,
+  capabilities = capabilities,
 }
